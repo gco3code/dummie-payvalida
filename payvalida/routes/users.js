f6ce37be851c6800248eb8aa26a62ce84816e12d
Cristian Lombana <clombana@gco.com.co>
2018-08-06 13:32:54 -0500

    m

--- payvalida/routes/users.js
+++ payvalida/routes/users.js
@@ -45,7 +45,7 @@ router.get('/consultaBaloto', function(req, res, next) {
       .then((res) => {
           res.rows.forEach(function(row){
             //console.log(row.cedula);
-            var postData = querystring.stringify({"cedula":"87069371"});
+            var postData = JSON.stringify({"cedula":"87069371"});
             //enviar la cedula para consultar
             var postObject = constructPostDataConsulta(postData);
 
@@ -106,7 +106,7 @@ router.get('/consultaBaloto', function(req, res, next) {
                 //otras constantes
               })
             });
-            
+
             request.write(postData);
             request.end();
 
